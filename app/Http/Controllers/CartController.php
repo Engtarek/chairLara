7a927e9d9a4ad2169098ec67eb56addce91cd974
hendsabry66 <sabryhend170@gmail.com>
2017-05-16 23:39:28 +0300

    fix25

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -94,7 +94,7 @@ class CartController extends Controller
              $image_id.=$array[$x];
            }
             $image = ProductLayerImage::find($image_id);
-            $total+=intval($image->item_price);
+            //$total+=intval($image->item_price);
             //$total+=$image->item_price;
             // if($i+1 == count($items)){
             //     $url .=$array[0].'.'.$array[1];
