d4e2f77cc9ec0f6452f17161f83934ded84fa179
hendsabry66 <sabryhend170@gmail.com>
2017-05-18 05:29:35 +0300

    fix46

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -7,6 +7,12 @@
   <link rel="stylesheet" type="text/css" href="\css\jssocials.css" />
   <link rel="stylesheet" type="text/css" href="\css\jssocials-theme-minima.css" />
   <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
+  <script>
+     $(document).bind('mobileinit',function(){
+         $.mobile.keepNative = "select,input,div";
+     });
+ </script>
+ <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
 @endsection
 
@@ -96,12 +102,7 @@
   </div>
 @endsection
 @section('script')
- <script>
-    $(document).bind('mobileinit',function(){
-        $.mobile.keepNative = "select,input,div";
-    });
-</script>
-<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
     <script src="/js/jssocials.min.js"></script>
     <script src="/js/chair.js"></script>
     <script src="/js/custom.js"></script>
