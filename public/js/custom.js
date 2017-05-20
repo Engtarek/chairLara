fee1734d3adcb5d14af0eab0008312ed3bb3f89d
hendsabry66 <sabryhend170@gmail.com>
2017-05-20 22:54:33 +0300

    fix51

--- public/js/custom.js
+++ public/js/custom.js
@@ -12,9 +12,8 @@ function chair(param2){
       }else if(e.type == 'touchstart'){
           startPoint=e.originalEvent.touches[0].clientX;
       }
-
-
   });
+
   $('.parent').on('mousemove touchmove',function (e) {
       if(e.type == 'mousemove'){
         if(down){
@@ -24,11 +23,11 @@ function chair(param2){
                 if(x===-2100){
                     x=0;
                     y-=700;
-                    $(this).css('background-position',x +'px '+y+'px');
+                    $(this).find('.'+param2+'').css('background-position',x +'px '+y+'px');
 
                 }else{
                     x-=700;
-                    $(this).css('background-position',x +'px '+y+'px');
+                    $(this).find('.'+param2+'').css('background-position',x +'px '+y+'px');
                 }
 
             }else if((e.clientX-startPoint)+offset<0){
@@ -36,16 +35,16 @@ function chair(param2){
               if(x===0){
                   x-=2100;
                   y-=700;
-                  $(this).css('background-position',x +'px '+y+'px');
+                  $(this).find('.'+param2+'').css('background-position',x +'px '+y+'px');
 
               }else{
                   x+=700;
-                  $(this).css('background-position',x +'px '+y+'px');
+                  $(this).find('.'+param2+'').css('background-position',x +'px '+y+'px');
               }
             }
 
-  }
-}else if(e.type == 'touchmove'){
+          }
+      }else if(e.type == 'touchmove'){
         if(down){
             if((e.originalEvent.touches[0].clientX-startPoint)-offset>0){
 
@@ -53,11 +52,11 @@ function chair(param2){
                 if(x===-2100){
                     x=0;
                     y-=700;
-                    $(this).css('background-position',x +'px '+y+'px');
+                    $(this).find('.'+param2+'').css('background-position',x +'px '+y+'px');
 
                 }else{
                     x-=700;
-                    $(this).css('background-position',x +'px '+y+'px');
+                    $(this).find('.'+param2+'').css('background-position',x +'px '+y+'px');
                 }
 
             }else if((e.originalEvent.touches[0].clientX-startPoint)+offset<0){
@@ -65,65 +64,21 @@ function chair(param2){
               if(x===0){
                   x-=2100;
                   y-=700;
-                  $(this).css('background-position',x +'px '+y+'px');
+                  $(this).find('.'+param2+'').css('background-position',x +'px '+y+'px');
 
               }else{
                   x+=700;
-                  $(this).css('background-position',x +'px '+y+'px');
+                  $(this).find('.'+param2+'').css('background-position',x +'px '+y+'px');
               }
             }
 
   }
-
       }
+
   });
   $('.parent').on('mouseup touchend',function (e) {
       down=false;
       startPoint=null;
   });
-
-//   let down=false;
-//   let startPoint=null;
-//   let x=0;
-//   let y=0;
-//   let offset=25;
-//   $('.parent').on('mousedown',function (e) {
-//       down=true;
-//       startPoint=e.clientX;
-//   });
-//   $('.parent').on('mousemove',function (e) {
-//       if(down){
-//           if((e.clientX-startPoint)-offset>0){
-//
-//               startPoint=e.clientX;
-//               if(x===-2100){
-//                   x=0;
-//                   y-=700;
-//                   $(this).find('.'+param2+'').css('background-position',x +'px '+y+'px');
-//
-//               }else{
-//                   x-=700;
-//                   $(this).find('.'+param2+'').css('background-position',x +'px '+y+'px');
-//               }
-//
-//           }else if((e.clientX-startPoint)+offset<0){
-//             startPoint=e.clientX;
-//             if(x===0){
-//                 x-=2100;
-//                 y-=700;
-//                 $(this).find('.'+param2+'').css('background-position',x +'px '+y+'px');
-//
-//             }else{
-//                 x+=700;
-//                 $(this).find('.'+param2+'').css('background-position',x +'px '+y+'px');
-//             }
-//           }
-//
-// }
-//   });
-//   $('.parent').on('mouseup touchend',function (e) {
-//       down=false;
-//       startPoint=null;
-//   });
 
 }
--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -7,11 +7,11 @@
   <link rel="stylesheet" type="text/css" href="\css\jssocials.css" />
   <link rel="stylesheet" type="text/css" href="\css\jssocials-theme-minima.css" />
   <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
-  <script>
+  <!-- <script>
      $(document).bind('mobileinit',function(){
          $.mobile.keepNative = "select,input,div";
      });
- </script>
+ </script> -->
  <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
 @endsection
