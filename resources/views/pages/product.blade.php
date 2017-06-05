176b7bd0f7fa897217c71b435b3c3233c8bc6bc3
hendsabry66 <sabryhend170@gmail.com>
2017-06-05 10:50:55 +0300

    fix62

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -157,10 +157,10 @@
        var spinner = new Spinner().spin(target);
        $.ajax({url: "/change_image/"+product_id+"/"+ch_layer_id2+"", success: function(result){
          $('.chair').css('background-image','url(/products/'+product_id+'/history/'+result+'.png)');
+           $('.chair').load(function(){
+               $('#foo').hide();
+           });
 
-         $(window).load(function() {
-            $('#foo').hide();
-          });
         }
       });
       social(window.location.href,product_name);
