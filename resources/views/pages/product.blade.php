97ca81eeae06fb5901e2ab5698d46b13c7a26df5
hendsabry66 <sabryhend170@gmail.com>
2017-06-05 10:38:41 +0300

    fix61

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -152,34 +152,15 @@
        }
        history.pushState(null, null,ch_layer_id);
        //change image
-       var opts = {
-         lines: 11 // The number of lines to draw
-   , length: 18 // The length of each line
-   , width: 7 // The line thickness
-   , radius: 25 // The radius of the inner circle
-   , scale: 0.5 // Scales overall size of the spinner
-   , corners: 1 // Corner roundness (0..1)
-   , color: '#000' // #rgb or #rrggbb or array of colors
-   , opacity: 0.35 // Opacity of the lines
-   , rotate: 0 // The rotation offset
-   , direction: 1 // 1: clockwise, -1: counterclockwise
-   , speed: 1.1 // Rounds per second
-   , trail: 60 // Afterglow percentage
-   , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-   , zIndex: 2e9 // The z-index (defaults to 2000000000)
-   , className: 'spinner' // The CSS class to assign to the spinner
-   , top: '50%' // Top position relative to parent
-   , left: '50%' // Left position relative to parent
-   , shadow: false // Whether to render a shadow
-   , hwaccel: false // Whether to use hardware acceleration
-   , position: 'absolute' // Element positioning
-       }
         $('#foo').show();
        var target = document.getElementById('foo')
-       var spinner = new Spinner(opts).spin(target);
+       var spinner = new Spinner().spin(target);
        $.ajax({url: "/change_image/"+product_id+"/"+ch_layer_id2+"", success: function(result){
          $('.chair').css('background-image','url(/products/'+product_id+'/history/'+result+'.png)');
-         $('#foo').hide();
+
+         $(window).load(function() {
+            $('#foo').hide();
+          });
         }
       });
       social(window.location.href,product_name);
@@ -214,11 +195,4 @@ social(window.location.href,product_name);
       $(".add-to-cart").attr("href", url+"/"+qty);
   });
        </script>
-
-
-
-
-
-
-
 @endsection
