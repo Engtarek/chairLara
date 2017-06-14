f7d2d7be7483dd2b9d5278f1a6fa12c6547a024b
hendsabry66 <sabryhend170@gmail.com>
2017-06-14 11:26:58 +0300

    demo

--- public/js/change-image.js
+++ public/js/change-image.js
@@ -1,6 +1,7 @@
 function change_image(product_id,ch_layer_id2,img_pos){
 
   $.ajax({url: "/change_image/"+product_id+"/"+ch_layer_id2+"/"+img_pos, success: function(result){
+    console.log(result);
   //  $('.chair').css('background-image','url('/products/'+product_id+'/small_image/'+result+'.jpg')');
       $('.chair').css('background-image','url(/products/'+product_id+'/small_image/'+result+'.jpg)');
     $('#load').css('display','none');
