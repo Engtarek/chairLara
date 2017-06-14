193f2b51af858b6af3542024409dcf210ddf11f1
hendsabry66 <sabryhend170@gmail.com>
2017-06-14 11:31:33 +0300

    demo

--- public/js/change-image.js
+++ public/js/change-image.js
@@ -2,16 +2,16 @@ function change_image(product_id,ch_layer_id2,img_pos){
 
   $.ajax({url: "/change_image/"+product_id+"/"+ch_layer_id2+"/"+img_pos, success: function(result){
     console.log(result);
-  //  $('.chair').css('background-image','url('/products/'+product_id+'/small_image/'+result+'.jpg')');
-      $('.chair').css('background-image','url(/products/'+product_id+'/small_image/'+result+'.jpg)');
-    $('#load').css('display','none');
-
-    let img=new Image();
-    img.onload=function(){
-      $('.chair').css('background-image','url('+$(this).attr("src")+')');
-      }
-    img.src='/products/'+product_id+'/history/'+result+'.jpg';
-   },
+  // //  $('.chair').css('background-image','url('/products/'+product_id+'/small_image/'+result+'.jpg')');
+  //     $('.chair').css('background-image','url(/products/'+product_id+'/small_image/'+result+'.jpg)');
+  //   $('#load').css('display','none');
+  //
+  //   let img=new Image();
+  //   img.onload=function(){
+  //     $('.chair').css('background-image','url('+$(this).attr("src")+')');
+  //     }
+  //   img.src='/products/'+product_id+'/history/'+result+'.jpg';
+  //  },
  });
 
 }
