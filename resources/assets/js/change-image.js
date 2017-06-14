624f4440e13274b31ab125ca5581f2cb3b13b028
hendsabry66 <sabryhend170@gmail.com>
2017-06-14 12:45:34 +0300

    demo1

--- resources/assets/js/change-image.js
+++ resources/assets/js/change-image.js
@@ -1,14 +1,19 @@
 window.change_image=function(product_id,ch_layer_id2,img_pos){
 
   $.ajax({url: "/change_image/"+product_id+"/"+ch_layer_id2+"/"+img_pos, success: function(result){
-      $('.chair').css('background-image','url(/products/'+product_id+'/small_image/'+result+'.jpg)');
-    $('#load').css('display','none');
 
+let sm_img =new Image();
+  sm_img.onload=function(){
+    $('.chair').css('background-image','url('++$(this).attr("src")++')');
+    $('#load').css('display','none');
     let img=new Image();
     img.onload=function(){
       $('.chair').css('background-image','url('+$(this).attr("src")+')');
       }
     img.src='/products/'+product_id+'/history/'+result+'.jpg';
+
+  }
+    img.src='/products/'+product_id+'/small_image/'+result+'.jpg';
 
 }
  });
