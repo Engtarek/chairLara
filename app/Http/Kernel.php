ccb67e4dd38e8a49fa342b8aba816f0820aec292
hendsabry66 <sabryhend170@gmail.com>
2017-08-10 14:35:55 +0300

    demo7

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,13 +35,11 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Language::class,
-            \App\Http\Middleware\Cros::class,
         ],
 
         'api' => [
             'throttle:60,1',
             'bindings',
-            \App\Http\Middleware\Cros::class,
         ],
     ];
 
