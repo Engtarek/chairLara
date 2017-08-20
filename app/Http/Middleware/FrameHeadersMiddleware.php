8a08491b12afd815c5bca4860b29a6a863487f13
hendsabry66 <sabryhend170@gmail.com>
2017-08-20 18:17:24 +0300

    demo7

--- app/Http/Middleware/FrameHeadersMiddleware.php
+++ app/Http/Middleware/FrameHeadersMiddleware.php
@@ -16,6 +16,7 @@ class FrameHeadersMiddleware
     public function handle($request, Closure $next)
     {
       $response = $next($request);
+      $response->header->remove('X-Frame-Options');
      $response->header('X-Frame-Options', 'ALLOW FROM http://198.199.122.78.com/');
      return $response;
 
